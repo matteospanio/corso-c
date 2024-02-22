a9d6799525c3e283b5e57afc476cd0b942abe8b0
Matteo Spanio <dev2@audioinnova.com>
2024-02-22 17:20:00 +0100

    first commit

---
+++ lessons/01/code/format_strings.c
@@ -0,0 +1,14 @@
+int main(void) {
+  char c_val = 'm';
+  int i_val = 255;
+  long int l_val = 99;
+  float f_val = 25.8;
+  
+  printf("%c", c_val);
+  printf("%d", i_val);
+  printf("%ld", l_val);
+  printf("%x", i_val);
+  printf("%f", f_val);
+  printf("%g", f_val);
+  printf("%s", "hello");
+}
